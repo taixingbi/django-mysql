a76bd1436d7442711e48a29f5a91722d8fa722ef
taixingbi <bitaihang@gmail.com>
2021-06-16 11:44:54 -0400

    Create serializers.py

---
+++ database/serializers.py
@@ -0,0 +1,8 @@
+# from rest_framework import serializers
+# from .models import User
+# from rest_framework import filters
+
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['name', 'age']
